bb7439f032fbc100b6fe5df9503d903164861b2f
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-08 20:51:44 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Telkom [081251549899]
-│ • Gopay  [081352848936]
-│ • Dana  [081251549899]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Gopay  [083160327945]
+│ • Dana  [083848128003]
+│ • Saweria  [https://saweria.co/asepbotz]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281251549899
+│ > Ingin donasi? Wa.me/6283160327945
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
