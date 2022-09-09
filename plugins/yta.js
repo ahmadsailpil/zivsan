54febc4ed32dd59bbda87d940bb8109cb55f46cb
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-09 08:22:30 +0800

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,41 +1,76 @@
-let limit = 30
-let fetch = require('node-fetch')
-const { servers, yta } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
+/*let limit = 50
+// const { servers, yta } = require('../lib/y2mate')
+const { youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-┏┉━━━━━━━━━━━❏
-┆ *YOUTUBE MP3*
-├┈┈┈┈┈┈┈┈┈┈┈
-┆• *Judul:* ${title}
-│• *Type:* MP3
-┆• *📥 Ukuran File:* ${filesizeF}
-└❏
+  // let server = (args[1] || servers[0]).toLowerCase()
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0]))
+  let audio, link = '', lastError
+  for (let i in _audio) {
+    try {
+      audio = _audio[i]
+      link = await audio.download()
+      if (link) break
+    } catch (e) {
+      lastError = e
+      continue
+    }
+  }
+  if (!link) throw lastError
+  // let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < audio.fileSize
+  if (!isY) conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+📌*Title:* ${title}
+🗎 *Filesize:* ${audio.fileSizeH}
+*${isLimit ? 'Pakai ' : ''}Link:* ${link}
+`.trim(), m)
+  if (!isLimit) conn.sendFile(m.chat, link, title + '.mp3', `
+📌 *Title:* ${title}
+🗎 *Filesize:* ${audio.fileSizeH}
 `.trim(), m, null, {
-    asDocument: chat.useDocument, mimetype: 'audio/mp4', ptt: true, contextInfo: {
-        externalAdReply: {
-            title: '▶︎ ━━━━━━━•────────────────── ', 
-            body: 'Now Playing...',
-            description: 'Now Playing...',
-            mediaType: 2,
-          thumbnail: await (await fetch(thumb)).buffer(),
-         mediaUrl: `https://youtube.com/watch?v=uIedYGN3NQQ`
-        }
-     }
+    asDocument: chat.useDocument
   })
 }
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
+handler.tags = ['downloader']
+handler.command = /^yt(a|mp3)$/i
+
+handler.exp = 0
+
+module.exports = handler*/
+
+
+
+
+let limit = 30
+const { servers, yta } = require('../lib/y2mate')
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*Title♨️:* ${title}
+*Filesize♨️:* ${filesizeF}
+*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
+`.trim(), m)
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+`.trim(), m, null, {
+  asDocument: chat.useDocument
+})
+}
+handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
 
 handler.admin = false
@@ -46,3 +81,11 @@ handler.exp = 0
 handler.limit = true
 
 module.exports = handler
+
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}
+
+
+
