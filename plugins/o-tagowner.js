c4d100060582638e918bd85158e457d017450898
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-09 09:08:47 +0800

    Update o-tagowner.js

--- plugins/o-tagowner.js
+++ plugins/o-tagowner.js
@@ -9,7 +9,7 @@ Gak usah Malu-malu
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@62895336282144 |@62895336282144/i
+handler.customPrefix = /@6283160327945 |@6283848128003/i
 handler.command = new RegExp
 
 module.exports = handler
